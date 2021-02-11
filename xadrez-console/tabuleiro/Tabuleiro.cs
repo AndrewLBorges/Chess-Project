1e92aaf49a282f554676cd28368bc3afc07b2b86
Andrew Borges <andrewborges71@gmail.com>
2021-02-11 14:48:37 -0300

    Fazendo os primeiros testes de movimentacao

--- xadrez-console/tabuleiro/Tabuleiro.cs
+++ xadrez-console/tabuleiro/Tabuleiro.cs
@@ -40,6 +40,18 @@ namespace tabuleiro
             p.Posicao = pos;
         }
 
+        public Peca RetirarPeca(Posicao pos)
+        {
+            if(PegaPeca(pos) == null)
+            {
+                return null;
+            }
+            Peca aux = PegaPeca(pos);
+            aux.Posicao = null;
+            _pecas[pos.Linha, pos.Coluna] = null;
+            return aux;
+        }
+
         public bool PosicaoValida(Posicao pos)
         {
             if (pos.Linha < 0 || pos.Linha >= Linhas || pos.Coluna < 0 || pos.Coluna >= Colunas)
