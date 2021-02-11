63fec1ac7719b2e68c340a4f8b6dcd8a1b444e19
Andrew Borges <andrewborges71@gmail.com>
2021-02-11 10:00:51 -0300

    Tratando erros com excecao personalizada

--- xadrez-console/tabuleiro/Tabuleiro.cs
+++ xadrez-console/tabuleiro/Tabuleiro.cs
@@ -19,10 +19,42 @@ namespace tabuleiro
             return _pecas[linha, coluna];
         }
 
+        public Peca PegaPeca(Posicao pos)
+        {
+            return _pecas[pos.Linha, pos.Coluna];
+        }
+
+        public bool ExistePeca(Posicao pos)
+        {
+            ValidarPosicao(pos);
+            return PegaPeca(pos) != null;
+        }
+
         public void ColocarPeca(Peca p, Posicao pos)
         {
+            if(ExistePeca(pos))
+            {
+                throw new TabuleiroException("Já existe uma peça nessa posição!");
+            }
             _pecas[pos.Linha, pos.Coluna] = p;
             p.Posicao = pos;
+        }
+
+        public bool PosicaoValida(Posicao pos)
+        {
+            if (pos.Linha < 0 || pos.Linha >= Linhas || pos.Coluna < 0 || pos.Coluna >= Colunas)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public void ValidarPosicao(Posicao pos)
+        {
+            if(!PosicaoValida(pos))
+            {
+                throw new TabuleiroException("Posição inválida!");
+            }
         }
     }
 }
---
+++ xadrez-console/tabuleiro/TabuleiroException.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace tabuleiro
+{
+    class TabuleiroException : ApplicationException
+    {
+        public TabuleiroException(string msg) : base(msg) { }
+    }
+}
